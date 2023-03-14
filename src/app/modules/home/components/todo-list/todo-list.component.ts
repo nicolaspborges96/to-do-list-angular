e38237733c2c32431d385bced291ce10c75b490a
Nicolas Borges <nicolaspborges96@gmail.com>
2023-03-14 16:56:31 -0300

    to do finalizando

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -9,9 +9,24 @@ import { TaskList } from '../../model/task-list';
 export class TodoListComponent {
 
     public taskList: Array<TaskList> = [
-        
 
     ];
 
+    public setEmitTaskList(event: string) {
+        this.taskList.push({task: event, checked: false});
+    }
+
+    public deleteItemTaskList(event: number) {
+        this.taskList.splice(event, 1);
+    }
+
+    public deleteAllTaskList(){
+        const confirm = window.confirm('Você realmente deletar tudo?');
+
+        if(confirm) {
+            this.taskList = [];
+        }
+        
+    }
 
 }
